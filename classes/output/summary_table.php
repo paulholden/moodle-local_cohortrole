paulholden/moodle-local_cohortrole

commit 0b8d1512bba1195c07e04e1e7fa6c24e5e5ded33

Use persistent instances in summary table.

classes/output/summary_table.php
@@ -47,7 +47,7 @@ class summary_table extends \table_sql implements \renderable {
 
         // Table configuration.
         $this->set_attribute('cellspacing', '0');
-        $this->set_attribute('class', 'generaltable generalbox local-cohortrole-summary-table');
+        $this->set_attribute('class', $this->attributes['class'] .' local-cohortrole-summary-table');
 
         $this->sortable(true, 'timecreated', SORT_DESC);
         $this->no_sorting('edit');
@@ -65,13 +65,22 @@ class summary_table extends \table_sql implements \renderable {
      * @return void
      */
     protected function init_sql() {
-        global $DB;
+        $from = '{' . \local_cohortrole\persistent::TABLE . '}';
 
-        $fields = 'cr.id, cr.timecreated, cr.roleid, c.name AS cohort, r.shortname AS role';
-        $from = '{local_cohortrole} cr JOIN {cohort} c ON c.id = cr.cohortid JOIN {role} r ON r.id = cr.roleid';
-
-        $this->set_sql($fields, $from, '1=1');
+        $this->set_sql('*', $from, '1=1');
         $this->set_count_sql('SELECT COUNT(1) FROM ' . $from);
+    }
+
+    /**
+     * Format record cohort column
+     *
+     * @param stdClass $record
+     * @return string
+     */
+    public function col_cohort(\stdClass $record) {
+        $persistent = (new \local_cohortrole\persistent())->from_record($record);
+
+        return format_string($persistent->get_cohort()->name, true, \context_system::instance());
     }
 
     /**
@@ -81,11 +90,9 @@ class summary_table extends \table_sql implements \renderable {
      * @return string
      */
     public function col_role(\stdClass $record) {
-        global $DB;
+        $persistent = (new \local_cohortrole\persistent())->from_record($record);
 
-        $role = $DB->get_record('role', ['id' => $record->roleid], '*', MUST_EXIST);
-
-        return role_get_name($role, \context_system::instance(), ROLENAME_ALIAS);
+        return role_get_name($persistent->get_role(), \context_system::instance(), ROLENAME_ALIAS);
     }
 
     /**

classes/persistent.php
@@ -78,4 +78,26 @@ class persistent extends \core\persistent {
 
         return true;
     }
+
+    /**
+     * Returns the model cohort object
+     *
+     * @return stdClass
+     */
+    public function get_cohort() {
+        global $DB;
+
+        return $DB->get_record('cohort', ['id' => $this->get('cohortid')], '*', MUST_EXIST);
+    }
+
+    /**
+     * Returns the role object
+     *
+     * @return stdClass
+     */
+    public function get_role() {
+        global $DB;
+
+        return $DB->get_record('role', ['id' => $this->get('roleid')], '*', MUST_EXIST);
+    }
 }

commit 5cf443ae8241cba98a5d20c972859b2ca46d26e9

Fix table sorting.
Broken in 0b8d1512bba1195c07e04e1e7fa6c24e5e5ded33; cohort & role tables weren't
being included in query, so their columns couldn't be sorted on.

classes/output/summary_table.php
@@ -67,10 +67,37 @@ class summary_table extends \table_sql implements \renderable {
      * @return void
      */
     protected function init_sql() {
-        $from = '{' . persistent::TABLE . '}';
+        $fields = persistent::get_sql_fields('cr') . ', c.name AS cohort, r.shortname AS role';
 
-        $this->set_sql('*', $from, '1=1');
+        $from = '{' . persistent::TABLE . '} cr
+            JOIN {cohort} c ON c.id = cr.cohortid
+            JOIN {role} r ON r.id = cr.roleid';
+
+        $this->set_sql($fields, $from, '1=1');
         $this->set_count_sql('SELECT COUNT(1) FROM ' . $from);
+    }
+
+    /**
+     * Add alias to timecreated field prior to sorting
+     *
+     * @return string
+     */
+    public function get_sql_sort() {
+        $sort = parent::get_sql_sort();
+
+        return str_replace('timecreated', 'cr.timecreated', $sort);
+    }
+
+    /**
+     * Extract persistent record prior to formatting
+     *
+     * @param array|object $row
+     * @return array
+     */
+    public function format_row($row) {
+        $record = persistent::extract_record((object) $row);
+
+        return parent::format_row($record);
     }
 
     /**
@@ -80,7 +107,7 @@ class summary_table extends \table_sql implements \renderable {
      * @return string
      */
     public function col_cohort(\stdClass $record) {
-        $persistent = (new persistent())->from_record($record);
+        $persistent = new persistent(0, $record);
 
         return format_string($persistent->get_cohort()->name, true, \context_system::instance());
     }
@@ -92,7 +119,7 @@ class summary_table extends \table_sql implements \renderable {
      * @return string
      */
     public function col_role(\stdClass $record) {
-        $persistent = (new persistent())->from_record($record);
+        $persistent = new persistent(0, $record);
 
         return role_get_name($persistent->get_role(), \context_system::instance(), ROLENAME_ALIAS);
     }